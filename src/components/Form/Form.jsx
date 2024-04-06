a8c87a706ab0992c12f3cb847767ce914912db32
Roman Fedevych <fedevychr@gmail.com>
2024-04-06 19:15:29 +0300

    add form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,39 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const query = value.trim();
+    if (!query.length) return;
+
+    onSubmit(query);
+  };
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setValue(value);
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        value={value}
+        onChange={handleChange}
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,34 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, Text } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchPhotos = async () => {
+      try {
+        const data = await getPhotos(query, page);
+        console.log('data: ', data);
+        setPhotos(data.photos);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPhotos();
+  }, [page, query]);
+
+  const onSubmit = newQuery => {
+    setQuery(newQuery);
+  };
+
   return (
     <>
+      <Form onSubmit={onSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
